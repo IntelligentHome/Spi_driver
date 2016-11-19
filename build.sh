3153e7d7782a5c84210d9831c04b8a59449189bd
Mariusz Sikora <msikora87@gmail.com>
2016-11-19 19:46:11 +0100

    Change build.sh to google style

--- build.sh
+++ build.sh
@@ -1,78 +1,76 @@
 #!/bin/bash
 
-BUILD=0
-TEST=0
-CLEAN=0
+build=0
+run_tests=0
+clean=0
 
-EXIT_CODE=0
+exit_code=0
 
-BUILD_DIR=bin
-TEST_DIR=bin_tests
+readonly BUILD_DIR=bin
+readonly TEST_DIR=bin_tests
 
 function build_Spi_driver {
     local retval=0
 
-    mkdir -p $BUILD_DIR
-    cd $BUILD_DIR
+    mkdir -p ${BUILD_DIR}
+    cd ${BUILD_DIR}
     cmake ..
     make
     retval=$?
     cd ..
 
-    return $retval
+    return ${retval}
 }
 
 function run_tests {
     local retval=0
 
-    mkdir -p $TEST_DIR
-    cd $TEST_DIR
+    mkdir -p ${TEST_DIR}
+    cd ${TEST_DIR}
     cmake ../tests
     make
     ./Spi_driver_unit_tests
     retval=$?
     cd ..
 
-    return $retval
+    return ${retval}
 }
 
 function clean {
-    rm -r -f $BUILD_DIR
-    rm -r -f $TEST_DIR
+    rm -r -f ${BUILD_DIR}
+    rm -r -f ${TEST_DIR}
 }
 
 while getopts ":btc" opt; do
-    case $opt in
-        b)
-            BUILD=1 ;;
-        t)
-            TEST=1 ;;
-        c)
-            CLEAN=1 ;;
+    case ${opt} in
+        b) build=1      ;;
+        t) run_tests=1  ;;
+        c) clean=1      ;;
         \?)
-            echo "Invalid option: -$OPTARG" >$2
+            echo "Invalid option: -${OPTARG}" >$2
             exit 1 ;;
     esac
 done
 
-if [ 1 == $BUILD ]; then
+if [ 1 == ${build} ]; then
     build_Spi_driver
     if [ $? != 0 ]; then
-        $EXIT_CODE=1
+        exit_code=1
     fi
 fi
 
-if [ 1 == $TEST ]; then
+if [ 1 == ${run_tests} ]; then
     run_tests
     if [ $? != 0 ]; then
-        $EXIT_CODE=1
+        exit_code=1
     fi
 fi
 
-if [ 1 == $CLEAN ]; then
+if [ 1 == ${clean} ]; then
     clean
     if [ $? != 0 ]; then
-        $EXIT_CODE=1
+        exit_code=1
     fi
 fi
 
+exit ${exit_code}
